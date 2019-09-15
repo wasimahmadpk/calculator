635c73b42477453ef4a2e35478dbe506dbc9f2dc
Wasim <wasimahmad.qc@gmail.com>
2019-09-15 21:19:09 +0200

    new commit

--- calculator.py
+++ calculator.py
@@ -6,11 +6,11 @@ Calculator library containing basic math operations.
 class calculator:
 
     def add(first_term, second_term):
-        return first_term + second_term
+        return first_term + second_term + "\n"
 
 
     def sub(first_term, second_term):
-        return first_term - second_term
+        return first_term - second_term + "\n"
 
     def multiply(first_term, second_term):
-        return first_term * second_term
+        return first_term * second_term + "\n"
