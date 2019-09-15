dd2af77b48a81cc9dbc9ed5c00f299a42f4cc0bc
Wasim <wasimahmad.qc@gmail.com>
2019-09-15 21:41:12 +0200

    commit config

--- calculator.py
+++ calculator.py
@@ -1,13 +1,11 @@
 # Calculator library containing basic math operations
 
 
-class calculator:
+def add(first_term, second_term):
+    return first_term + second_term
 
-    def add(first_term, second_term):
-        return first_term + second_term
+def sub(first_term, second_term):
+    return first_term - second_term
 
-    def sub(first_term, second_term):
-        return first_term - second_term
-
-    def multiply(first_term, second_term):
-        return first_term * second_term
+def multiply(first_term, second_term):
+    return first_term * second_term
--- test_calculator.py
+++ test_calculator.py
@@ -1,16 +1,16 @@
 # Unit test for calculator
 
 
-from calculator import calculator
+import calculator
 
 
 class TestCalculator:
 
     def test_add(self):
-        assert 4 == calculator.calculator.add(2, 2)
+        assert 4 == calculator.add(2, 2)
 
     def test_sub(self):
-        assert 2 == calculator.calculator.sub(4, 2)
+        assert 2 == calculator.sub(4, 2)
 
     def test_multiplication(self):
-        assert 100 == calculator.calculator.multiply(10, 10)
+        assert 100 == calculator.multiply(10, 10)
