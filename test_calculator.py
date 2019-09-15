d91d3687bf9245a32fce116e67186f7fd554abde
Wasim <wasimahmad.qc@gmail.com>
2019-09-15 18:48:24 +0200

    test1 added

---
+++ test_calculator.py
@@ -0,0 +1,16 @@
+""" 
+Unit test for calculator
+"""
+
+import calcultor
+
+
+Class TestCalculator:
+
+	def test_addition(self):
+		assert 5 == calcultor.addition(2, 3)
+
+
+	def test_substraction(self):
+		assert 4 == calcultor.substraction(9, 5)
+
