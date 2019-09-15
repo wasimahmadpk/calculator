d60e8939cf7506895a70fbd35229c3e5527d3570
Wasim <wasimahmad.qc@gmail.com>
2019-09-15 19:12:40 +0200

    changes

--- calculator.py
+++ calculator.py
@@ -7,5 +7,5 @@ def add(first_term, second_term):
     return first_term + second_term
 
 
-def subtract(first_term, second_term):
+def sub(first_term, second_term):
     return first_term - second_term
--- test_calculator.py
+++ test_calculator.py
@@ -2,16 +2,16 @@
 Unit test for calculator
 """
 
-import calcultor
+import calculator
 
 
 class TestCalculator:
 
 	def test_addition(self):
-		assert 5 == calcultor.addition(2, 3)
+		assert 5 == calculator.add(2, 3)
 
 
 	def test_substraction(self):
 
-		assert 4 == calcultor.substraction(9, 5)
+		assert 4 == calculator.sub(9, 5)
 
