605ecec12fe4ac5e02687fc66865bcab9a80bf73
Wasim <wasimahmad.qc@gmail.com>
2019-10-26 23:53:16 +0200

    division functionality test added

--- test_calculator.py
+++ test_calculator.py
@@ -14,3 +14,6 @@ class TestCalculator:
 
     def test_multiplication(self):
         assert 100 == calculator.multiply(10, 10)
+
+    def test_division(self):
+    	assert 5 == calculator.divide(50, 10)
